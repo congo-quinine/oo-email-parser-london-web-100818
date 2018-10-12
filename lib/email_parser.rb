2f1bbc8d01f011ceb297c5d3d9b24b78f3ca9ce5
Rob Parker <into.the.parker@gmail.com>
2018-10-12 08:20:27 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,12 +3,11 @@ class EmailParser
   attr_accessor :parse
   
   def initialize(email_list)
-    @email_list = email_list
+    @email = email_list
   end  
-  
-  def self.parse 
-    return @email_list.split(' , ').uniq
-  end
 
+  def parse 
+    @email 
+  end
 
 end
